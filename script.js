d4091d1b6be965fe9d3d302ed52cd964c1ecb4b7
gifcandra <gifari.candra08@gmail.com>
2026-08-06 11:22:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -160,16 +160,16 @@ contactForm.addEventListener("submit", (e) => {
 
   // Ganti nomor ini dengan nomor WhatsApp kamu
   // Format: 62 + nomor tanpa angka 0 di depan
-  const nomorWhatsApp = "6281953399673";
+  const nomorWhatsApp = "+6281584069960";
 
-  const teksPesan = `Halo Robby, saya ${nama}.
+  const teksPesan = `Halo Gifari, saya ${nama}.
 
 Email saya: ${email}
 
 Pesan:
 ${pesan}`;
 
-  const urlWhatsApp = `https://wa.me/${nomorWhatsApp}?text=${encodeURIComponent(teksPesan)}`;
+  const urlWhatsApp = `https://wa.me/${+6281584069960}?text=${encodeURIComponent(Assalamualaikum, apa kabar pak?)}`;
 
   window.open(urlWhatsApp, "_blank");
 
